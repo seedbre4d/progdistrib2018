db88db7690a7c900995f10ae54be49ff3ed358dd
Dragoș Iliescu <dragos@abosolutions.nl>
2018-06-05 20:59:14 +0300

    stateless working

--- Sum/ejb/stateless/src/java/ejb/SumBean.java
+++ Sum/ejb/stateless/src/java/ejb/SumBean.java
@@ -1,33 +1,15 @@
 package sum.ejb;
 import javax.ejb.Stateless;
+import java.util.stream.IntStream;
  
 @Stateless
 public class SumBean{
-  public String sum(long num){
-     String sir = "Sirul este:";
-		StringBuilder sb = new StringBuilder (String.valueOf (sir));
-		sb.append(num);
-		sb.append(",");	
-        int count = 0;
-        while(num!=1)
-        {
-            if(num%2==0)
-            {
-                num /= 2;	
-				sb.append(num);
-				sb.append(",");					
-            }
-            else
-            {
-                num = 3*num + 1;
-				sb.append(num);
-				sb.append(",");
-                count++;
-            }
-        }
-		sb.append(" [nr pasi =");
-		sb.append(count);
-		sb.append("]");
+    public String sum(int n) {
+        Integer sum = IntStream.range(1, n)
+                .filter(i -> i % 3 == 0 || i % 5 == 0)
+                .sum();
+        StringBuilder sb = new StringBuilder();
+        sb.append(sum);
         return sb.toString();
   }   
 }
